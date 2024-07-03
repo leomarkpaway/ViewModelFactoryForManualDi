450a803a9866dfd4509cd97117daa74fc8022fe1
leomarkpaway <leomarkpaway@gmail.com>
2024-07-03 15:54:29 +0800

    :sparkles: Implement viewmodel with repository  dependency

--- app/src/main/java/com/leomarkpaway/viewmodelwithdependency/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewmodelwithdependency/MainActivity.kt
@@ -2,10 +2,18 @@ package com.leomarkpaway.viewmodelwithdependency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = Repository()
+        val factory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(viewModelStore, factory)[MainViewModel::class.java]
+
     }
 }
