f1fa361c1bcae9801134acdca61ad30d141734ee
leomarkpaway <leomarkpaway@gmail.com>
2024-08-01 19:29:17 +0800

    :art: Convert view model factory into generic factory

--- app/src/main/java/com/leomarkpaway/viewmodelwithdependency/ViewModelFactory.kt
+++ app/src/main/java/com/leomarkpaway/viewmodelwithdependency/ViewModelFactory.kt
@@ -3,13 +3,18 @@ package com.leomarkpaway.viewmodelwithdependency
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
-
+class ViewModelFactory<T : ViewModel>(
+    private val creator: () -> T
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(MainViewModel::class.java)) {
+        if (modelClass.isAssignableFrom(creator().javaClass)) {
             @Suppress("UNCHECKED_CAST")
-            return MainViewModel(repository) as T
+            return creator() as T
         }
         throw IllegalArgumentException("UNKNOWN VIEW MODEL CLASS")
     }
+}
+
+inline fun <reified T : ViewModel> createFactory(noinline creator: () -> T): ViewModelProvider.Factory {
+    return ViewModelFactory(creator)
 }
